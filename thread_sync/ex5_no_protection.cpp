4fd72673f1601375d7277c4c8c42e366edc199ad
marmal95 <marcin.malecki95@gmail.com>
2025-02-25 19:12:56 +0100

    Thread basics

---
+++ thread_sync/ex5_no_protection.cpp
@@ -0,0 +1,27 @@
+#include "../util/print.hpp"
+#include <array>
+#include <atomic>
+#include <iostream>
+#include <thread>
+
+std::atomic<int> data{0};
+std::array<int, 5> shared_data{};
+
+void do_work(int thread_num)
+{
+    auto val = data.fetch_add(1);
+    shared_data[thread_num] = val;
+}
+
+int main()
+{
+    {
+        std::jthread th0{do_work, 0};
+        std::jthread th1{do_work, 1};
+        std::jthread th2{do_work, 2};
+        std::jthread th3{do_work, 3};
+        std::jthread th4{do_work, 4};
+    }
+
+    print(shared_data);
+}
--- util/print.hpp
+++ util/print.hpp
@@ -1,11 +1,30 @@
 #pragma once
 
+#include <array>
 #include <iostream>
 #include <syncstream>
 #include <thread>
+#include <vector>
 
-template <typename... Args>
-inline void print(Args &&...args)
+template <typename T> inline std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec)
+{
+    std::cout << "[ ";
+    for (const auto& elem : vec)
+        std::cout << elem << " ";
+    std::cout << "]";
+    return out;
+}
+
+template <typename T, std::size_t N> std::ostream& operator<<(std::ostream& out, const std::array<T, N>& arr)
+{
+    out << "[ ";
+    for (const auto& elem : arr)
+        out << elem << " ";
+    out << "]";
+    return out;
+}
+
+template <typename... Args> inline void print(Args&&... args)
 {
     std::osyncstream out{std::cout};
     out << "[" << std::this_thread::get_id() << "]: ";
