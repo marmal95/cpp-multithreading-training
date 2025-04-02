f83960aea812f089f496bcb8248adba3b52722c7
marmal95 <marcin.malecki95@gmail.com>
2025-04-02 15:29:41 +0200

    Condition variable

---
+++ 4. thread_coordination/ex5_condition_variable.cpp
@@ -0,0 +1,54 @@
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+std::queue<int> dataQueue;
+std::mutex mtx;
+std::condition_variable cv;
+bool finished = false;
+
+void producer()
+{
+    for (int i = 1; i <= 5; ++i)
+    {
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            dataQueue.push(i);
+            std::cout << "Produced: " << i << std::endl;
+        }
+        cv.notify_one(); // Notify consumer
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        finished = true;
+    }
+    cv.notify_one(); // Notify consumer to terminate
+}
+
+void consumer()
+{
+    while (true)
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [] { return !dataQueue.empty() || finished; });
+
+        while (!dataQueue.empty())
+        {
+            int value = dataQueue.front();
+            dataQueue.pop();
+            std::cout << "Consumed: " << value << std::endl;
+        }
+
+        if (finished)
+            break;
+    }
+}
+
+int main()
+{
+    std::jthread producerThread(producer);
+    std::jthread consumerThread(consumer);
+}
